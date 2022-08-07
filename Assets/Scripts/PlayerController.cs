4573d65e662c6dab114e7037898ef6dfbcf098b4
WhoMrNobody <the.emrehun@gmail.com>
2022-08-07 17:40:18 +0300

    First uploaded of the RPG Core Combat Game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Movement;
+using UnityEngine;
+
+namespace RPG.Control{
+public class PlayerController : MonoBehaviour
+{
+    
+   void Update()
+    {   
+        if(Input.GetMouseButton(0)){
+
+            MoveToCursor();
+           
+        }
+        
+    }
+
+    void MoveToCursor(){
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        
+        if(hasHit){
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+
+    }
+ }
+}
